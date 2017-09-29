86defad3f9be6e18327693cf350d00f71ccb4bb3
BelRoy <oksiwhite@ukr.net>
2017-09-29 15:44:16 +0300

    Need to prepare project

---
+++ app/src/main/java/com/devqt/ardweather/StartScreen.java
@@ -0,0 +1,13 @@
+package com.devqt.ardweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+    }
+}
